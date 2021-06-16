0d06765ef149150f6f324c621a0f392076dcf434
quaini <quain.i@husky.neu.edu>
2021-06-16 12:29:33 -0400

    logs error until I import numpy

--- engines/gold_engine.py
+++ engines/gold_engine.py
@@ -5,6 +5,7 @@ import copy
 from strategies import MinimalEngine
 from engine_wrapper import EngineWrapper
 import logging
+#import numpy as np
 
 logger = logging.getLogger(__name__)
 logging.error("here1")
@@ -15,12 +16,14 @@ class Node:
         self.wins, self.visits  = 0, 0
 
     def expand_node(self, board):
+        logging.error("in expand_node")
         if not board.is_game_over():
           for m in list(board.legal_moves):
             nc = Node(m, self) # new child node
             self.children.append(nc)
 
     def update(self, result):
+        logging.error("in update")
         self.visits += 1
         if result.is_variant_loss():
             self.wins += 0
@@ -30,9 +33,11 @@ class Node:
             self.wins += .5
 
     def is_leaf(self):
+        logging.error("in is_leaf")
         return len(self.children)==0
 
     def has_parent(self):
+        logging.error("in has_parent")
         return self.parent is not None
 
 
@@ -41,8 +46,10 @@ class GoldEngine(MinimalEngine):
 
     """
     def mcts(self, board, start_time):
+        logging.error("in mcts")
         root_node = Node(None, None)
         while self.time_remaining(start_time):
+            logging.error("time to calc")
             n, b = root_node, copy.deepcopy(board)
             while not n.is_leaf():    # select leaf
                 n = self.tree_policy_child(n)
@@ -61,14 +68,18 @@ class GoldEngine(MinimalEngine):
 
     # determines which node to visit with explore factor
     def tree_policy_child(self, n):
+        logging.error("in tree_policy_child")
         best_explore_bonus = 0
         best_to_visit_node = Node(None, None)
-        for child in node.children:
-            explore_bonus = child.wins / child.visits + (sqrt(2) * sqrt(math.log(child.parent.visits) / child.visits))
+        for child in n.children:
+            logging.error("in tree_policy_child looking through children")
+            explore_bonus = child.wins / child.visits + (np.sqrt(2) * np.sqrt(math.log(child.parent.visits) / child.visits)) if child.visits > 0 else 100
+            logging.error("explore_bonus: {}".format(explore_bonus))
             if (explore_bonus > best_explore_bonus):
                 best_explore_bonus = explore_bonus
                 best_to_visit_node = child
 
+        logging.error("best_to_visit_node: {}".format(best_to_visit_node))
         return best_to_visit_node
 
     """
@@ -81,25 +92,28 @@ class GoldEngine(MinimalEngine):
         hasTime (boolean): is there still time to run best move calculation
     """
     def time_remaining(self, start_time):
+        logging.error("in time_remaining")
         #challenge = config["challenge"]
         # board.time controls
-        return time.time() - start_time > 5
+        return time.time() - start_time < 5
 
     def simulation_policy_child(self, b):
-        b.push(random.choice(list(board.legal_moves)))
+        logging.error("in simulation_policy_child")
+        b.push(random.choice(list(b.legal_moves)))
         return b
 
     def best_move(self, root_node):
+        logging.error("in best_move")
         most_visits = 0
         best_move = chess.Move.from_uci("g1f3")
         for node in root_node.children:
+            logging.error("in best_move looking through children")
             if node.visits > most_visits:
                 most_visits = node.visits
                 best_move = node.move
-                print("Best Move " + str(best_move))
         return best_move
 
     def search(self, board, *args):
-        logging.error("here search")
+        logging.error("in search")
         start_time = time.time()
         return self.mcts(board, start_time)
