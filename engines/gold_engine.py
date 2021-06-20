56bd1e5cd48a1fbce6570adb6583328c88367ec0
VedanshiShah7 <56982656+VedanshiShah7@users.noreply.github.com>
2021-06-20 19:27:03 -0400

    Update gold_engine.py
    it works!

--- engines/gold_engine.py
+++ engines/gold_engine.py
@@ -8,25 +8,23 @@ import logging
 import math
 import chess.pgn
 
-logger = logging.getLogger(__name__)
-
 class Node:
     """
     A Node represents a chess state for the monte-carlo tree search data structure.
     """
 
     def __init__(self, m, p, b): # move is from parent to node
-    """ Initialises a node for the monte-carlo tree search tree.
+        """ Initialises a node for the monte-carlo tree search tree.
 
-    Attributes:
-        self (Node): The current node
-        m (chess.move): Represents the move to get to this node from the parent node
-        p (Node): The parent node
-        b (chess.board): The game state of the board
+        Attributes:
+            self (Node): The current node
+            m (chess.move): Represents the move to get to this node from the parent node
+            p (Node): The parent node
+            b (chess.board): The game state of the board
 
-    Return:
+        Return:
 
-    """
+        """
         self.move, self.parent, self.children = m, p, []
         self.wins, self.visits  = 0, 0
         self.board = b
@@ -49,9 +47,11 @@ class Node:
           for m in list(self.board.legal_moves):
             b = copy.deepcopy(self.board)
             b.push(m)
+
             nc = Node(m, self, b)
             if m not in moves_added:
                 self.children.append(nc)
+
                 return nc
 
     def update(self, result, engine):
@@ -99,6 +99,7 @@ class Node:
         Return:
             (Boolean): If the node has a parent
         """
+
         return self.parent is not None
 
 
@@ -131,12 +132,10 @@ class GoldEngine(MinimalEngine):
             b = copy.deepcopy(n.board)
             leaf_nodes.append(n)
             move_count = 0
-
             while not b.is_game_over() and move_count < 10:     # simulation
                 b = self.simulation_policy_child(b)
                 move_count += 1
             result = b
-
             while n.has_parent():                               # backpropagation
                 n.update(result, self)
                 n = n.parent
@@ -159,14 +158,13 @@ class GoldEngine(MinimalEngine):
         best_to_visit_node = Node(None, None, None)
 
         for child in leaf_nodes:
-            explore_bonus = child.wins / child.visits + (math.sqrt(2) * math.sqrt(math.log(child.parent.visits) / child.visits)) if (child.visits > 0 and child.parent.visits > 0) else 100
 
+            explore_bonus = child.wins / child.visits + (math.sqrt(2) * math.sqrt(math.log(child.parent.visits) / child.visits)) if (child.visits > 0 and child.parent.visits > 0) else 100
             if (explore_bonus > best_explore_bonus and child.is_leaf()):
                 best_explore_bonus = explore_bonus
                 best_to_visit_node = child
 
         return best_to_visit_node, leaf_nodes
-
 
     def time_remaining(self, start_time, time_limit):
         """ Determines if there is more time available for the bot to calculate next best move
@@ -220,7 +218,6 @@ class GoldEngine(MinimalEngine):
                 white_count += 9
             elif c == 'q':
                 black_count += 9
-
         return (white_count - black_count) / (white_count + black_count)  if b.turn == chess.WHITE else (black_count - white_count) / (white_count + black_count)
 
     def simulation_policy_child(self, b):
@@ -236,7 +233,6 @@ class GoldEngine(MinimalEngine):
 
         best_move = None
         best_move_score = -100
-
         for move in list(b.legal_moves):
             temp_b = copy.deepcopy(b)
             temp_b.push(move)
@@ -262,10 +258,8 @@ class GoldEngine(MinimalEngine):
         most_visits = 0
         best_move = chess.Move.from_uci("g1f3")
         logging.error("---------\n")
-
         for node in root_node.children:
             logging.error("Move: {}, Visit Count: {}, Win Ratio: {}".format(node.move, node.visits, node.wins / node.visits))
-
             if node.visits > most_visits:
                 most_visits = node.visits
                 best_move = node.move
